069368e1fa45e2e1d1f42300857e9aec31783293
Rodrigo Souza <rodrigo.ses6@gmail.com>
2023-07-02 13:12:04 -0300

    add readme

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function toggleMode() {
 
   if (html.classList.contains('light')) {
     
-   img.setAttribute("src", "./assets/assets/avatar-light.png")
+   img.setAttribute("src", "./assets/avatar-light.png")
   }
   else {
     
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 
  
